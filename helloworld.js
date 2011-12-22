b1c7d2ba69102e61bccf8af04cbe9b332abcef8d
Fiona Hall-Zazueta <fhall.zazueta@gmail.com>
2011-12-21 21:47:13 -0800

    first commit

---
+++ helloworld.js
@@ -0,0 +1,15 @@
+/*
+	function: hello 
+	author: Fiona
+	date: 12-21-11
+	hello prints out the message, "Hello, World!"
+*/
+
+function hello() {
+
+	//say hello to the world
+	var msg = "Hello, <em>World!</em>";
+	document.open();
+	document.writeln(msg);
+	document.close();
+}
